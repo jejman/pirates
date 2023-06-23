19e9bdc39ad60c36c3df3a76972a7dcf55312225
jejman <134877489+jejman@users.noreply.github.com>
2023-06-23 09:11:14 -0400

    jwje
    jeje

---
+++ pirategame javascrip.js
@@ -0,0 +1,16 @@
+
+// const input=document.querySelector("input")
+
+
+
+function storeInformation() {
+    let  userInput = document.querySelector("input").value;
+    console.log(userInput);
+
+    
+  }
+
+
+  const p=document.querySelector("p")
+
+  p.innerHTML=`${userInput}`
